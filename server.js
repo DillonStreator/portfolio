3a3232d73d2311ed119441658e2b0daf8607c09f
dillonstreator <dillonstreator@gmail.com>
2020-08-24 23:40:37 -0500

    add helmet for security headers

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const next = require("next");
+const helmet = require("helmet");
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app
+  .prepare()
+  .then(() => {
+    const server = express();
+    server.use(
+      helmet({
+        contentSecurityPolicy: {
+          directives: {
+            "default-src": ["'self'"],
+            "script-src": [
+              "'self'",
+              "https://www.google-analytics.com/analytics.js",
+            ],
+            "style-src": ["'self'", "fonts.googleapis.com 'unsafe-inline'"],
+            "font-src": ["'self'", "fonts.gstatic.com 'unsafe-inline'"],
+          },
+        },
+      })
+    );
+
+    server.get("*", (req, res) => {
+      return handle(req, res);
+    });
+
+    server.listen(8000, (err) => {
+      if (err) throw err;
+      console.log("> Ready on http://localhost:8000");
+    });
+  })
+  .catch((ex) => {
+    console.error(ex.stack);
+    process.exit(1);
+  });
